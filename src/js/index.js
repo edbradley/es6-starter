8c340c42dfd56a55a63870f29f22c8eb6e4613ff
Edward Bradley <edbrad@edbradleyweb.net>
2019-02-16 09:05:42 -0600

    Initial Model and Controller setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,39 @@
-// applicaion - MVC - Controller module
+// APPLICATION - MVC - Controller MODULE
 
 // include other files w/ webpack entry point
 import "../css/styles.css";
-import "../assets/static/favicon.ico"
+import "../assets/static/favicon.ico";
+
+// include app configuration
+import { zipCode } from "./config";
 
 // include references to external module files (Model & View)
-import x from "./models/Sample";
-import y from "./views/sampleView";
+import Sample from "./models/Sample";
+
+/**
+ * GLOBAL APP STATE
+ */
+const state = {};
 
 // test access to external modules
-console.log('ES6-starter - webpack workflow')
-console.log(`Imported ${x} from Sample Module!`);
-console.log(`Imported ${y} from sampleView Module!`)
+console.log("ES6-starter - webpack workflow");
+
+/**
+ * SAMPLE CONTROLLER
+ */
+const controlSample = async () => {
+  state.weather = new Sample(zipCode);
+
+  try {
+    state.weather.data = await state.weather.getTodaysWeather();
+    console.log(`Weather Data: ${JSON.stringify(state.weather.data)}`);
+  } catch (error) {
+    console.log(error);
+    alert(`There was an error: ${error}`);
+  }
+  
+};
+
+controlSample();
+
 
--- src/js/models/Sample.js
+++ src/js/models/Sample.js
@@ -1,3 +1,32 @@
-// application - MVC - Model module
-console.log("Sample Model Module!");
-export default "Sample Model module string";
+// APPLICATION - MVC - Sample Model MODULE
+
+// include application configuration
+import { apiKey_openweathermap_org } from "../config";
+
+// http client
+import axios from "axios";
+
+/**
+ * ============
+ * Sample Class
+ * ============
+ */
+export default class Sample {
+  constructor(zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  /**
+   * asynchronus Internet API call (Get Weather Data)
+   */
+  async getTodaysWeather() {
+    try {
+      const response = await axios(`http://api.openweathermap.org/data/2.5/weather?zip=${this.zipCode},us&APPID=${apiKey_openweathermap_org}`);
+      console.log(`API Call Response (data): ${JSON.stringify(response.data)}`);
+      return response.data;
+    } catch (error) {
+        alert(`There was an API Call Error: ${error}`);
+        console.log(error);
+    }
+  }
+}
